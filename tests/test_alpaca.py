ea9705030f88d48733ad85a088223adb25d1a5bc
TexasCoding <jeff10278@me.com>
2024-05-11 17:28:44 -0500

    cadded instructions for testing

--- tests/test_alpaca.py
+++ tests/test_alpaca.py
@@ -8,6 +8,11 @@ from pytz import timezone
 from py_alpaca_api.alpaca import PyAlpacaApi
 from py_alpaca_api.src.data_classes import AccountClass, AssetClass, OrderClass
 
+# The following keys are for testing purposes only
+# You should never hardcode your keys in your code
+# Instead, you should use environment variables
+# to store your keys and access them in your code
+# Create a .env file in the root directory of the project for the following:
 api_key = os.environ.get("API_KEY")
 api_secret = os.environ.get("API_SECRET")
 
