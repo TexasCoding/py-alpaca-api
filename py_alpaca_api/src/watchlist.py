3a413344b0f68b395505ac0588d1613a114f9271
TexasCoding <jeff10278@me.com>
2024-05-14 05:36:56 -0500

    added imports

--- py_alpaca_api/src/watchlist.py
+++ py_alpaca_api/src/watchlist.py
@@ -1,3 +1,9 @@
+# import json
+
+# import requests
+
+
 class Watchlist:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, trade_url: str, headers: object) -> None:
+        self.trade_url = trade_url
+        self.headers = headers
