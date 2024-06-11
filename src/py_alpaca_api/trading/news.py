71c905b01d43d1ec460826c9bc3cbefdedce8ed6
TexasCoding <jeff10278@me.com>
2024-06-11 04:57:31 -0500

    update

--- src/py_alpaca_api/trading/news.py
+++ src/py_alpaca_api/trading/news.py
@@ -173,7 +173,7 @@ class News:
                 - "url": The URL of the news article.
                 - "source": The source of the news article (in this case, "benzinga").
                 - "content": The content of the news article, or None if there is no content.
-                - "publish_date": The publish date of the news article.
+                - "publish_date": The publishing date of the news article.
                 - "symbol": The symbol associated with the news article.
         """
         url = f"{self.news_url}"
